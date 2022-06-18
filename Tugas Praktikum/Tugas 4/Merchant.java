605c72d40210d0fa51502dd8d8b9b81e4a1d4e08
Siti Waheeda Najihah <89684509+sitiwaheeda@users.noreply.github.com>
2022-06-18 21:44:37 +0700

    Add files via upload

---
+++ Tugas Praktikum/Tugas 4/Merchant.java
@@ -0,0 +1,37 @@
+package Tugas4; //siti waheeda najihah_215150701111046
+
+public class Merchant {
+    String namaMerchant, namaProduk;
+    double hargaMakanan;
+
+    public Merchant(String namaMerchant, String namaProduk, double hargaMakanan) {
+        this.namaMerchant = namaMerchant;
+        this.namaProduk = namaProduk;
+        this.hargaMakanan = hargaMakanan;
+    }
+
+    public String getNamaMerchant() {
+        return namaMerchant;
+    }
+
+    public void setNamaMerchant(String namaMerchant) {
+        this.namaMerchant = namaMerchant;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public double getHargaMakanan() {
+        return hargaMakanan;
+    }
+
+    public void setHargaMakanan(double hargaMakanan) {
+        this.hargaMakanan = hargaMakanan;
+    }
+}
+
